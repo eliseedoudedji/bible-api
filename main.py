bf280028ccb1b50171cd5f9636d0c14d844a87d5
edoudedji <ahouanganelisee@gmail.com>
2024-08-20 23:33:45 +0100

    creating app

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from typing import Optional
 
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+with open('bible.json', "r") as f:
+        data = json.load(f)
+   
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/chapitre/{id}")
+def read_item(id):
+    id= int(id)
+    chapter =[]
+    long= len(data['Testaments'][0]['Books'][0]["Chapters"][id]['Verses'])
+    for i in range(0,long):
+        chapter.append(data['Testaments'][0]['Books'][0]["Chapters"][id]['Verses'][i]['Text'])
+
+    return chapter
+
+@app.get("/livres/{id}")
+def read_item(id):
+    id= int(id)
+    chapter =[]
+    long= len(data['Testaments'][id]['Books'])
+    for i in range(0,long):
+        chapter.append(data['Testaments'][id]['Books'][i]['Text'])
+
+    return chapter
+
+@app.get("/testament/{id}")
+def read_item(id):
+    id= int(id)
+
+    chapter =[]
+    # long= len(data['Testaments'][id])
+    # for i in range(0,long):
+    #     chapter.append(data['Testaments'][0]['Books'][i]['Text'])
+
+    return data['Testaments'][id]['Text']
